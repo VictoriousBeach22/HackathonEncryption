872c91fa7c95f4842b5ce1c347fd18ad5b6dc6d8
akauntz <anastasiakauntz@gmail.com>
2017-10-21 10:13:45 -0400

    Started Main File

---
+++ EncryptData.java
@@ -0,0 +1,18 @@
+/**
+ * Put a short phrase describing the program here.
+ *
+ * @author Put your name here
+ *
+ */
+public final class EncryptData {
+
+    /**
+     * Private constructor so this utility class cannot be instantiated.
+     */
+    private EncryptData() {
+    }
+
+public static void main(String[] args) {
+
+}
+}
